d37b8d228f43b003ad44386209f79cc7281ece9b
julkwel <jrajerison@bocasay.com>
2023-08-05 15:28:34 +0300

    add property extractor

--- src/Helpers/JsonResponseHelper.php
+++ src/Helpers/JsonResponseHelper.php
@@ -6,10 +6,14 @@
 
 namespace App\Helpers;
 
+use Symfony\Component\PropertyInfo\Extractor\PhpDocExtractor;
+use Symfony\Component\PropertyInfo\Extractor\ReflectionExtractor;
+use Symfony\Component\PropertyInfo\PropertyInfoExtractor;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Mapping\Factory\ClassMetadataFactory;
 use Symfony\Component\Serializer\Mapping\Loader\AnnotationLoader;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Component\Serializer\Normalizer\ArrayDenormalizer;
 use Symfony\Component\Serializer\Normalizer\DateTimeNormalizer;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Serializer;
@@ -38,9 +42,10 @@ class JsonResponseHelper
             },
         ];
 
+        $extractor = new PropertyInfoExtractor([], [new PhpDocExtractor(), new ReflectionExtractor()]);
         $classMetadataFactory = new ClassMetadataFactory(new AnnotationLoader());
-        $objectNormalizer = new ObjectNormalizer($classMetadataFactory, null, null, null, null, null, $defaultContext);
-        $normalizer = [new DateTimeNormalizer(), $objectNormalizer];
+        $objectNormalizer = new ObjectNormalizer($classMetadataFactory, null, null, $extractor, null, null, $defaultContext);
+        $normalizer = [new ArrayDenormalizer(), new DateTimeNormalizer(), $objectNormalizer];
 
         return new Serializer($normalizer, [$encoder]);
     }
